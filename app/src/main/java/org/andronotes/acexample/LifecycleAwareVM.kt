a94cfa281e3d7f2d3af6360df049c3d63e4bc408
Anton Derevyanko <anton.derevyanko@gmail.com>
2017-08-19 15:26:03 +0300

    Checkbox and button are connected via onCheckChanged listener

--- app/src/main/java/org/andronotes/acexample/LifecycleAwareVM.kt
+++ app/src/main/java/org/andronotes/acexample/LifecycleAwareVM.kt
@@ -1,10 +1,19 @@
 package org.andronotes.acexample
 
+import android.databinding.ObservableBoolean
+import android.widget.CompoundButton
+
 /**
  * Created by anton on 8/18/17.
  */
 class LifecycleAwareVM {
 
-    var isAgreeChecked = false
+    private var isAgreeChecked = true
+    var isButtonEnabled = ObservableBoolean(false)
+
+    fun checkedChangedListener(buttonView: CompoundButton, isChecked: Boolean) {
+        isAgreeChecked = isChecked
+        isButtonEnabled.set(isChecked)
+    }
 
 }
