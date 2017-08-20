32d7bafaf6fd2fda300c1ee78ba460cc6d623a0c
Anton Derevyanko <anton.derevyanko@gmail.com>
2017-08-20 13:37:15 +0300

    Checkbox behaviour is re-implemented via binding and boolean property

--- app/src/main/java/org/andronotes/acexample/LifecycleAwareVM.kt
+++ app/src/main/java/org/andronotes/acexample/LifecycleAwareVM.kt
@@ -1,19 +1,11 @@
 package org.andronotes.acexample
 
 import android.databinding.ObservableBoolean
-import android.widget.CompoundButton
 
 /**
  * Created by anton on 8/18/17.
  */
 class LifecycleAwareVM {
 
-    private var isAgreeChecked = true
-    var isButtonEnabled = ObservableBoolean(false)
-
-    fun checkedChangedListener(buttonView: CompoundButton, isChecked: Boolean) {
-        isAgreeChecked = isChecked
-        isButtonEnabled.set(isChecked)
-    }
-
+    var isConditionsAccepted = ObservableBoolean(false)
 }
